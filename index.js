51909fe99fd493200cb3e549b1e408d65f01fc49
Gil Ontiveros <luisgil9900@gmail.com>
2024-04-01 20:09:01 -0700

    css resposivo y funcion de reset

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ document.querySelector("form").addEventListener("submit", formCalculate);
 // const height = parseFloat(document.getElementById("height").value);
 // const age = parseFloat(document.getElementById("age").value);
 // const PAF = parseFloat(document.getElementById("PAF").value);
-
+document.querySelector("form").addEventListener("reset", () => {
+  document.getElementById("results").innerHTML = `<div>
+  <p id="BMP">Metabolismo basal:</p>
+  <p id="TEE">Calorias de mantenimiento:</p>
+  <p>Calorias para volumen:</p>
+  <p>Calorias para definir:</p>
+  </div>
+`;
+});
 // Funciones
 function formCalculate(e) {
   const data = Object.fromEntries(new FormData(e.target));
@@ -19,11 +27,17 @@ function formCalculate(e) {
     data.PAF,
     data.gender
   );
-  const BMP = (document.getElementById(
-    "BMP"
-  ).textContent = `Metabolismo basal: ${cals.BM} kcals`);
-  const TEE = (document.getElementById(
-    "TEE"
-  ).textContent = `Calorias de mantenimiento: ${cals.TEE} kcals`);
+
+  const results = document.getElementById("results");
+
+  const resultHTML = `<div>
+          <p id="BMP">Metabolismo basal:  ${cals.BM} kcals</p>
+          <p id="TEE">Calorias de mantenimiento:  ${cals.TEE} kcals</p>
+          <p>Calorias para volumen:  ${cals.TEE + 500.0} kcals</p>
+          <p>Calorias para definir:  ${cals.TEE - 500.0} kcals</p>
+          </div>
+  `;
+  results.innerHTML = resultHTML;
+
   e.preventDefault();
 }
